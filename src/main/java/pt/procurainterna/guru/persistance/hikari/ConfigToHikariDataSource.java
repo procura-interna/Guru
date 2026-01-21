7651bf24a560ae9d93f86e4dbc2e2038169a4435
procura-interna <127160648+procura-interna@users.noreply.github.com>
2026-01-21 01:57:49 +0000

    Return HikariDataSource instead of DataSource
    Changed the dataSource method to return HikariDataSource instead of the generic DataSource. This provides more specific type information and may allow callers to access Hikari-specific features.

--- src/main/java/pt/procurainterna/guru/persistance/hikari/ConfigToHikariDataSource.java
+++ src/main/java/pt/procurainterna/guru/persistance/hikari/ConfigToHikariDataSource.java
@@ -1,7 +1,5 @@
 package pt.procurainterna.guru.persistance.hikari;
 
-import javax.sql.DataSource;
-
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
@@ -9,7 +7,7 @@ import pt.procurainterna.guru.persistance.JdbcConfig;
 
 public class ConfigToHikariDataSource {
 
-  public DataSource dataSource(final JdbcConfig jdbcConfig) {
+  public HikariDataSource dataSource(final JdbcConfig jdbcConfig) {
     final HikariConfig config = new HikariConfig();
     config.setJdbcUrl(jdbcConfig.url);
     config.setUsername(jdbcConfig.user);
